0d2fd4590a30740c2c7de36e3960eb85281ff1b5
JinxJane <Dianajanemolina2@gmail.com>
2025-03-28 14:46:30 +0800

    Changekotoparaokayna

--- resources/js/components/landing/LinkList.jsx
+++ resources/js/components/landing/LinkList.jsx
@@ -6,12 +6,12 @@ const logo = '/image/CompanyLogo.jpg';
 
 const LinkList = () => {
     const links = [
-        { name: 'National Integrated Basic Research Agenda', url: '#system1' },
-        { name: 'Commissioned Research', url: '#system2' },
-        { name: 'Kapakanan Ng Tao Sa Oras Ng Pandemya - COVID', url: '#system3' },
-        { name: 'System 4', url: '#system4' },
-        { name: 'System 5', url: '#system5' },
-        { name: 'System 6', url: '#system6' },
+        { url: '#system1' },
+        { url: '#system2' },
+        { url: '#system3' },
+        { url: '#system4' },
+        { url: '#system5' },
+        { url: '#system6' },
     ];
 
     return (
@@ -21,14 +21,13 @@ const LinkList = () => {
                 <div className={styles['linklist-items']}>
                     {links.map((link, index) => (
                         <div key={index} className={styles['link-item-container']}>
-                            <img 
-                                src={logo} 
-                                alt="Company Logo" 
-                                className={styles['link-item-logo']} 
-                                onError={(e) => { e.target.src = '/image/placeholder.jpg'; }}
-                            />
                             <a href={link.url} className={styles['linklist-item']}>
-                                {link.name}
+                                <img 
+                                    src={logo} 
+                                    alt="Company Logo" 
+                                    className={styles['link-item-logo']} 
+                                    onError={(e) => { e.target.src = '/image/placeholder.jpg'; }}
+                                />
                             </a>
                         </div>
                     ))}
